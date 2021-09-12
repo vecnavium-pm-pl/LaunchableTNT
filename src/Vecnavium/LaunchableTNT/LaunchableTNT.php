49ffde1568093ed33cf804d0f6b097fb89800429
Vecnavium <83182010+Vecnavium@users.noreply.github.com>
2021-09-12 14:06:44 -0700

    Moving the code to Main.php

--- src/Vecnavium/LaunchableTNT/LaunchableTNT.php
+++ src/Vecnavium/LaunchableTNT/LaunchableTNT.php
@@ -2,18 +2,44 @@
 
 namespace Vecnavium\LaunchableTNT;
 
-use pocketmine\entity\Entity;
-use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\item\Item;
 use pocketmine\plugin\PluginBase;
-use Vecnavium\LaunchableTNT\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\block\Solid;
+use pocketmine\entity\Entity;
+use pocketmine\item\Item;
+use pocketmine\math\Vector3;
+use pocketmine\event\Listener;
+use pocketmine\entity\object\FallingBlock;
+use pocketmine\event\entity\EntityExplodeEvent;
+use pocketmine\entity\object\PrimedTNT;
 
-class LaunchableTNT extends PluginBase
-{
+class LaunchableTNT extends PluginBase implements Listener{
 
-    public function onEnable(): void
-    {
-        $this->getServer()->getPluginManager()->registerEvents(new TaskHandler($this), $this);
+    public function onEnable(): void{
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
     }
+
+    public function onExplode(EntityExplodeEvent $event): void
+    {
+        foreach ($event->getBlockList() as $block) {
+            if ($block instanceof Solid) {
+                $nbt = Entity::createBaseNBT($block);
+                $nbt->setInt("TileID", $block->getId());
+                $nbt->setByte("Data", $block->getDamage());
+            }
+        }
+    }
+
+    public function onInteract(PlayerInteractEvent $event): void
+    {
+        $player = $event->getPlayer();
+        if ($player->getInventory()->getItemInHand()->getId() === Item::TNT) {
+            $entity = Entity::createEntity("PrimedTNT", $player->getLevel(), Entity::createBaseNBT($player));
+            $entity->setMotion($player->getDirectionVector()->normalize()->multiply(2));
+            $entity->spawnToAll();
+        }
+    }
+
 
 }
